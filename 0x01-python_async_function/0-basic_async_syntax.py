fbd417c0d9578a66a0fb0ac78955687570041f30
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-08 17:25:16 +0100

    update

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-
+""" asynchronous coroutine for the integer argument"""
 import asyncio
 import random
+
 
 async def wait_random(max_delay = 10):
     delay = random.uniform(10, max_delay)

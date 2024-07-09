68616aa369689f31ccf7f5b61a5f919a668a71a0
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-09 18:56:04 +0100

    async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,4 +8,4 @@ async def async_generator():
     """ function for generating the async loops """
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(1.0, 10.0)
+        yield random.random() * 10

a97655368dcf55079aa6985a89808a1d18f6e1a2
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-29 19:49:30 +0100

    test github repos

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """ Github client"""
+
+
 import unittest
 from unittest.mock import patch, MagicMock
 from fixtures import public_repos_fixtures, expected_repos_with_apache_license
@@ -10,19 +12,19 @@ class TestGithubOrgClient(unittest.Testcase):
     """ a class that test github client """
     @patch("client.GithubOrgClient._public_repos_data",
            return_value=public_repos_fixture)
-    def test_public_repos(self, mock_repos):
+    def test_public_repos(self, mock_repos: MagicMock) -> None:
         """Test public repos """
-        client = GithubOrgClient('test_org')
-        result = client.public_repos()
-        expected_result = [repo['name'] for repo in public_repos_fixture]
+        client: GithubOrgClient = GithubOrgClient('test_org')
+        result: List[str] = client.public_repos()
+        expected_result: List[str] = [repo['name'] for repo in public_repos_fixture]
         self.assertEqual(result, expected_result)
 
     @patch('client.GithubOrgClient._public_repos_data',
            return_value=public_repos_fixture)
-    def test_public_repos_with_license(self, mock_repos):
+    def test_public_repos_with_license(self, mock_repos: MagicMock): -> None:
         """ test public repos with license """
-        client = GithubOrgClient("test_org")
-        result = client.public_repos(license='apache-2.0')
+        client: GithubOrgClient = GithubOrgClient("test_org")
+        result: List[str] = client.public_repos(license='apache-2.0')
         self.assertEqual(result, expected_repos_with_apache_license)
 
 

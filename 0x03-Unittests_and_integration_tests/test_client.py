432d1d6ebcaf52b1e2d614f65d4b380c61ed7725
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-29 19:36:56 +0100

    test github repos

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Github client"""
+import unittest
+from unittest.mock import patch, MagicMock
+from fixtures import public_repos_fixtures, expected_repos_with_apache_license
+from client import GithubOrgClient
+
+class TestGithubOrgClient(unittest.Testcase):
+    """ a class that test github client """
+    @patch("client.GithubOrgClient._public_repos_data",
+           return_value=public_repos_fixture)
+    def test_public_repos(self, mock_repos):
+        """Test public repos """
+        client = GithubOrgClient('test_org')
+        result = client.public_repos()
+        expected_result = [repo['name'] for repo in public_repos_fixture]
+        self.assertEqual(result, expected_result)
+
+    @patch('client.GithubOrgClient._public_repos_data',
+           return_value=public_repos_fixture)
+    def test_public_repos_with_license(self, mock_repos):
+        """ test public repos with license """
+        client = GithubOrgClient("test_org")
+        result = client.public_repos(license='apache-2.0')
+        self.assertEqual(result, expected_repos_with_apache_license)
+
+
+if __name__ == "__main__":
+        unittest.main()
